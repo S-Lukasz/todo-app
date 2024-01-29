4c8b02ea0a320d6e1623246aa9d0292aac7b7754
Łukasz Surma <90053744+S-Lukasz@users.noreply.github.com>
2024-01-30 00:05:50 +0100

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ToDo",
+  title: "Board",
   description: "Created by Łukasz Surma",
 };
 
